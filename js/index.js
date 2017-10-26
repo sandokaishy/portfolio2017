438cb4b2d05fa1602df3b1807c0682f864dbe294
Peter <sandokaishy@gmail.com>
2017-10-26 16:06:07 +0800

    create index animations
    adding js and css styling

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(function(){
+	$('.about').hover(function(){
+		$('.about_cover').toggleClass('active');
+	});
+	$('.works').hover(function(){
+		$('.works_cover').toggleClass('active');
+	});	
+
+});
